674fdcbf52f770992bcd08e7021c4eedb046c388
Drezno <61885206+Drezno@users.noreply.github.com>
2020-04-20 23:12:05 +0200

    Usunięcie dwóch ostatnich zer z unixtimestamp, dodanie paru metod pomocniczych konwertujących z liczb i na liczby, zmiana kodów na float

--- OgarniatorDanych_v3/Program.cs
+++ OgarniatorDanych_v3/Program.cs
@@ -117,10 +117,10 @@ namespace OgarniatorDanych_v3
                         while ((line = sr.ReadLine()) != null)
                         {
                             counter++;
-                            if (counter % 1000000 == 0)
+                            if (counter % 10000 == 0)
                             {
                                 Console.WriteLine("Przerobiono " + counter);
-                                //break;
+                                break;
                             }
 
                             /*if (line.Length != suggestedLineSize)
@@ -168,64 +168,129 @@ namespace OgarniatorDanych_v3
                 return 0;
             }
         }
+        //metody pomocnicze - zamieniające dane w liczby lub odwrotnie
 
-        public static double ConvertToUnixTimestamp(DateTime date)
+        private static double ConvertToUnixTimestamp(DateTime date)
         {
             DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             TimeSpan diff = date.ToUniversalTime() - origin;
             return Math.Floor(diff.TotalSeconds);
         }
 
+        private static string ConvertFromUnixTimestamp(string unix)
+        {//do poprawy - narazie daje według czasu GMT a powinno według polskiego +1h
+            long unixTime;
+            bool tryParse;
+            unix += "00";
+            string date;
+
+            tryParse = Int64.TryParse(unix, out unixTime);
+            if (!tryParse)
+                Console.WriteLine("Błąd konwersji z unix na date.");
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            date = epoch.AddSeconds(unixTime).ToString();
+
+            return date;
+        }
+
+        private static string ConvertPostalToNumber(string postal_code)
+        {
+            string ASCII = "";
+            foreach (char c in postal_code)
+            {
+                ASCII += System.Convert.ToInt32(c).ToString();
+            }
+            return ASCII;
+        }
+
         private static string ConvertCountryToNumber(string country)
         {
-            string ASCII="";
+            string ASCII = "";
             foreach (char c in country)
             {
-                ASCII+=System.Convert.ToInt32(c).ToString();
+                ASCII += System.Convert.ToInt32(c).ToString();
+            }
+            return ASCII;
+        }
+
+        private static string AddComma(string _string)
+        {
+            string floatNumber;
+            var builder = new StringBuilder();
+            int counter = 0;
+            foreach (var c in _string)
+            {
+                builder.Append(c);
+                if (++counter == 6)//po 6 znakach dodaje kropke do liczby zeby byl float
+                {
+                    builder.Append('.');
+                }
+            }
+            floatNumber = builder.ToString();
+            return floatNumber;
+        }
+
+        private static string RemoveComma(string _string)
+        {
+            _string = _string.Replace(".", "");
+            return _string;
+        }
+
+        private static string ConvertNumberToPostal(string postal_code)
+        {
+            string ASCII = "", temp = "0";
+            int counter = 0, unicode;
+            bool tryParse;
+            char character;
+
+            foreach (char c in postal_code)
+            {
+                temp += c;
+                counter++;
+                if (counter == 2)
+                {
+                    tryParse = Int32.TryParse(temp, out unicode);
+                    if (tryParse)
+                    {
+                        character = (char)unicode;
+                        ASCII += character.ToString();
+                        temp = "0";
+                        counter = 0;
+                    }
+                    else
+                        Console.WriteLine("Błąd konwersji");
+                }
             }
             return ASCII;
         }
 
         private static string ConvertNumberToCountry(string number)
         {
-            string ASCII = "", first_part="",second_part="";
+            string ASCII = "", temp = "0";
             int counter = 0, unicode;
             bool tryParse;
             char character;
 
             foreach (char c in number)
             {
-                if (counter < 2)
-                {
-                    first_part += c;
-                }
-                else if (counter >= 2)
-                {
-                    second_part += c;
-                }
+                temp += c;
                 counter++;
+                if (counter == 2)
+                {
+                    tryParse = Int32.TryParse(temp, out unicode);
+                    if (tryParse)
+                    {
+                        character = (char)unicode;
+                        ASCII += character.ToString();
+                        temp = "0";
+                        counter = 0;
+                    }
+                    else
+                        Console.WriteLine("Błąd konwersji");
+                }
             }
-            tryParse = Int32.TryParse(first_part,out unicode);
-            if (tryParse)
-            {
-                character = (char)unicode;
-                ASCII += character.ToString();
-            }
-            else
-                Console.WriteLine("Błąd konwersji");
-
-            tryParse = Int32.TryParse(second_part, out unicode);
-            if (tryParse)
-            {
-                character = (char)unicode;
-                ASCII += character.ToString();
-            }
-            else
-                Console.WriteLine("Błąd konwersji");
-
             return ASCII;
         }
-
 
         private static string FixLine(string line)
         {
@@ -238,7 +303,7 @@ namespace OgarniatorDanych_v3
 
             try
             {
-            //    string tempLine;
+                //    string tempLine;
                 //1 SHIPMENT_IDENTCODE
                 string tempLine = line.Substring(0, dlugosciNaglowkow[0]);
                 tempLine = tempLine.Replace(" ", "");
@@ -253,11 +318,13 @@ namespace OgarniatorDanych_v3
                 date = date.AddMinutes(-date.Minute).AddSeconds(-date.Second);//tylko godziny z danej daty - nie musimy znać czasu dostawy co do sekundy
                 tempLine = ConvertToUnixTimestamp(date).ToString();//konwertowanie nie do ticków (duża liczba) a do czasu unixowego
                 //tempLine = date.ToString();
-                //tempLine = date.Ticks.ToString();
+                //tempLine = date.Ticks.ToString();//interwal 100 nanosekund od 0001-01-01
                 //tempLine = tempLine.GetHashCode().ToString();
                 isNumber = Int32.TryParse(tempLine, out result);
                 if (!isNumber)
                     return null;
+                tempLine = tempLine.Substring(0, tempLine.Length - 2);//usunięcie dwóch ostatnich zer z unixtimestamp - żeby była mniejsza liczba przy uczeniu
+                //tempLine = ConvertFromUnixTimestamp(tempLine);
                 tempLine += ";";
                 if (counter < debugMax)
                 {
@@ -271,10 +338,11 @@ namespace OgarniatorDanych_v3
                 date = DateTime.ParseExact(tempLine, dateFormat, System.Globalization.CultureInfo.InvariantCulture);
                 date = date.AddMinutes(-date.Minute).AddSeconds(-date.Second);//tylko godziny z danej daty - nie musimy znać czasu dostawy co do sekundy
                 tempLine = ConvertToUnixTimestamp(date).ToString();//konwertowanie nie do ticków (duża liczba) a do czasu unixowego
-                //tempLine = date.Ticks.ToString();
+                //tempLine = date.ToString();
                 isNumber = Int32.TryParse(tempLine, out result);
                 if (!isNumber)
                     return null;
+                tempLine = tempLine.Substring(0, tempLine.Length - 2);//usunięcie dwóch ostatnich zer z unixtimestamp - żeby była mniejsza liczba przy uczeniu
                 tempLine += ";";
                 fixedLine += tempLine;
                 if (counter < debugMax)
@@ -288,10 +356,11 @@ namespace OgarniatorDanych_v3
                 date = DateTime.ParseExact(tempLine, dateFormat, System.Globalization.CultureInfo.InvariantCulture);
                 date = date.AddMinutes(-date.Minute).AddSeconds(-date.Second);//tylko godziny z danej daty - nie musimy znać czasu dostawy co do sekundy
                 tempLine = ConvertToUnixTimestamp(date).ToString();//konwertowanie nie do ticków (duża liczba) a do czasu unixowego
-                //tempLine = date.Ticks.ToString();
+                //tempLine = date.ToString();
                 isNumber = Int32.TryParse(tempLine, out result);
                 if (!isNumber)
                     return null;
+                tempLine = tempLine.Substring(0, tempLine.Length - 2);//usunięcie dwóch ostatnich zer z unixtimestamp - żeby była mniejsza liczba przy uczeniu
                 tempLine += ";";
                 fixedLine += tempLine;
                 if (counter < debugMax)
@@ -303,12 +372,16 @@ namespace OgarniatorDanych_v3
                 //5 RECIEVER_ZIP
                 tempLine = line.Substring(0, dlugosciNaglowkow[4]);
                 tempLine = tempLine.Replace(" ", "");
-                tempLine = tempLine.Replace("-", "");
+                //    tempLine = tempLine.Replace("-", "");
+                tempLine = ConvertPostalToNumber(tempLine);
                 //tylko kody pocztowe które są liczbowe - odrzuca kody NL i GB między innymi  //TODO: zrobić hasha (ale żeby potem można odhashować) tych kodów pocztowych które nie są liczbami
-                isNumber = Int32.TryParse(tempLine, out result);
+                isNumber = Int64.TryParse(tempLine, out long _result);
                 if (!isNumber)
                     return null;
                 //tempLine = tempLine.GetHashCode().ToString();
+                //tempLine = ConvertNumberToPostal(tempLine);
+                tempLine = AddComma(tempLine);
+                //tempLine = RemoveComma(tempLine);
                 tempLine += ";";
                 fixedLine += tempLine;
                 if (counter < debugMax)
@@ -337,12 +410,15 @@ namespace OgarniatorDanych_v3
                 //7 SENDER_ZIP
                 tempLine = line.Substring(0, dlugosciNaglowkow[6]);
                 tempLine = tempLine.Replace(" ", "");
-                tempLine = tempLine.Replace("-", "");
+                //tempLine = tempLine.Replace("-", "");
+                tempLine = ConvertPostalToNumber(tempLine);
                 //tylko kody pocztowe które są liczbowe - odrzuca kody NL i GB między innymi
-                isNumber = Int32.TryParse(tempLine, out result);
+                isNumber = Int64.TryParse(tempLine, out _result);
                 if (!isNumber)
                     return null;
+                //tempLine = ConvertNumberToPostal(tempLine);
                 //tempLine = tempLine.GetHashCode().ToString();
+                tempLine = AddComma(tempLine);
                 tempLine += ";";
                 fixedLine += tempLine;
                 if (counter < debugMax)
@@ -360,7 +436,7 @@ namespace OgarniatorDanych_v3
                     return null;
                 //tempLine += ConvertNumberToCountry(tempLine);
                 //tempLine = tempLine.GetHashCode().ToString();
-            //    tempLine += ";";
+                //    tempLine += ";";
                 fixedLine += tempLine;
                 if (counter < debugMax)
                 {
@@ -369,19 +445,19 @@ namespace OgarniatorDanych_v3
                 line = line.Substring(dlugosciNaglowkow[7], line.Length - dlugosciNaglowkow[7]);
 
                 //9 SHIPMENT_WEIGHT
-            //    tempLine = line.Substring(0, dlugosciNaglowkow[8]);
-            //    tempLine = tempLine.Replace(" ", "");
-            //    isNumber = Double.TryParse(tempLine, out double result_double);
-            //    if (!isNumber)
-            //        return null;
+                //    tempLine = line.Substring(0, dlugosciNaglowkow[8]);
+                //    tempLine = tempLine.Replace(" ", "");
+                //    isNumber = Double.TryParse(tempLine, out double result_double);
+                //    if (!isNumber)
+                //        return null;
                 //tempLine = tempLine.GetHashCode().ToString();
                 //tempLine += ";";
-            //    fixedLine += tempLine;
-            //    if (counter < debugMax)
-            //    {
-            //        System.Console.WriteLine(tempLine);
-            //    }
-            //    line = line.Substring(dlugosciNaglowkow[8], line.Length - dlugosciNaglowkow[8]);
+                //    fixedLine += tempLine;
+                //    if (counter < debugMax)
+                //    {
+                //        System.Console.WriteLine(tempLine);
+                //    }
+                //    line = line.Substring(dlugosciNaglowkow[8], line.Length - dlugosciNaglowkow[8]);
 
                 if (counter < debugMax)
                 {
@@ -413,3 +489,4 @@ namespace OgarniatorDanych_v3
         }
     }
 }
+
